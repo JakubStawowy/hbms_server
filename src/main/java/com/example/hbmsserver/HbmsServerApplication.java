bef236d855f72080c404ece3d45de71ccbddb52e
Jakub <stawowykuba@gmail.com>
2022-10-11 21:27:56 +0200

    Setting up environment.

---
+++ src/main/java/com/example/hbmsserver/HbmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hbmsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbmsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbmsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hbmsserver/HbmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hbmsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
