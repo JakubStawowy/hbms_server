60d4899ed3c8ef58bdfb1e02f34dfc7adce414cb
Jakub <stawowykuba@gmail.com>
2022-12-06 20:40:06 +0100

    JWT Authorization repaired.

--- src/main/java/com/example/hbmsserver/security/JwtFilter.java
+++ src/main/java/com/example/hbmsserver/security/JwtFilter.java
@@ -1,4 +1,53 @@
 package com.example.hbmsserver.security;
 
-public class JwtFilter {
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.impl.TextCodec;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+public class JwtFilter extends BasicAuthenticationFilter {
+    private final String secret;
+
+    public JwtFilter(AuthenticationManager authenticationManager,String secret) {
+        super(authenticationManager);
+        this.secret = secret;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            String header = httpServletRequest.getHeader("authorization");
+            UsernamePasswordAuthenticationToken authResult = getAuthenticationByToken(header);
+            SecurityContextHolder.getContext().setAuthentication(authResult);
+        } catch (ClaimJwtException | NullPointerException e) {
+            httpServletResponse.sendError(HttpStatus.FORBIDDEN.value());
+            return;
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthenticationByToken(String header) {
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(TextCodec.BASE64.decode(secret))
+                .parseClaimsJws(header.replace("Bearer ", ""));
+
+        String userId = claimsJws.getBody().get("userID").toString();
+        String role = claimsJws.getBody().get("role").toString();
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority(role));
+
+        return new UsernamePasswordAuthenticationToken(userId, null, authorities);
+    }
 }
--- src/main/java/com/example/hbmsserver/security/WebSecurityConfig.java
+++ src/main/java/com/example/hbmsserver/security/WebSecurityConfig.java
@@ -1,4 +1,44 @@
 package com.example.hbmsserver.security;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/*").authenticated()
+                .antMatchers("/budgets/**").hasRole("PREMIUM")
+                .and()
+                .addFilter(new JwtFilter(authenticationManager(), secret));
+
+        http.cors();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/login",
+                "/register",
+                "/",
+                "/csrf",
+                "/swagger-ui.html",
+                "/v2/api-docs",
+                "/webjars/**",
+                "/swagger-resources/**");
+    }
 }
